2fbea7bc7b2353eb67e74c28a33d954259217f5c
Tony Tsai <ttony427@gmail.com>
2024-07-13 09:51:59 +0800

    Add project files.

---
+++ ModelBindingDemo/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+
+namespace ModelBindingDemo.Models
+{
+    public class Student
+    {
+        [Required]
+        [BindRequired]
+        public required string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
